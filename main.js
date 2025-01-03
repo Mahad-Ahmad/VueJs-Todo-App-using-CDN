87a8727b8bc899040535407eec87fcc6521f403c
Mahad Ahmad <mahadahmad921@gmail.com>
2025-01-03 08:57:38 +0500

    create app file added

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import TodoApp from './todoApp.js'
 const { createApp } = Vue
 
 const app = createApp(TodoApp)
-app.mount('#app2')
-app.mount('#app1')
+createApp(TodoApp).mount('#app1')
+createApp(TodoApp).mount('#app2')
 
